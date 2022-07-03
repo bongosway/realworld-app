11589315714611d0a31102a563528c7de087ba3f
Edirin Atumah <bongosway@users.noreply.github.com>
2022-07-03 11:00:58 +0100

    Scaffolding (#1)
    * Scaffolding
    
    Add SpringBoot
    Add initial test
    
    * Switch to semantic versioning
    
    Add Developer detalls
    Add Source control details
    Add License details

---
+++ src/test/java/com/bongosway/io/realworldapp/RealWorldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bongosway.io.realworldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
